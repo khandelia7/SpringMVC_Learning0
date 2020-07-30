df355b99f873a97977fb0fce03c99385080be89d
khandelia7 <khandelia7@gmail.com>
2020-07-30 11:12:51 +0530

    Application Context

--- SpringDemo0/src/main/java/com/telusko/SpringDemo0/App.java
+++ SpringDemo0/src/main/java/com/telusko/SpringDemo0/App.java
@@ -1,8 +1,10 @@
 package com.telusko.SpringDemo0;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+//import org.springframework.beans.factory.BeanFactory;
+//import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import org.springframework.core.io.FileSystemResource;
 
 /**
  * Hello world!
@@ -10,7 +12,8 @@ import org.springframework.core.io.FileSystemResource;
  */
 public class App {
     public static void main( String[] args ) {
-    	BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
     	Alien obj = (Alien)factory.getBean("alien");
         System.out.println( "Hello World!" );
         obj.code();
