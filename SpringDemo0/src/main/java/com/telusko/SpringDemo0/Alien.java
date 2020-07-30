289a74ad209c27fc1e3dcc13fc88c7d423af15d9
khandelia7 <khandelia7@gmail.com>
2020-07-30 11:16:17 +0530

    Spring Container

--- SpringDemo0/src/main/java/com/telusko/SpringDemo0/Alien.java
+++ SpringDemo0/src/main/java/com/telusko/SpringDemo0/Alien.java
@@ -2,6 +2,8 @@ package com.telusko.SpringDemo0;
 
 public class Alien {
 	
+	int age;
+	
 	public void code() {
 		System.out.println("I am Coding...!");
 	}
--- SpringDemo0/src/main/java/com/telusko/SpringDemo0/App.java
+++ SpringDemo0/src/main/java/com/telusko/SpringDemo0/App.java
@@ -13,9 +13,18 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main( String[] args ) {
     	//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+    	
     	ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
-    	Alien obj = (Alien)factory.getBean("alien");
+    	
+    	Alien obj1 = (Alien)factory.getBean("alien");
         System.out.println( "Hello World!" );
-        obj.code();
+        obj1.code();
+        obj1.age = 15;
+        System.out.println(obj1.age);
+        
+        Alien obj2 = (Alien)factory.getBean("alien");
+        System.out.println( "Hello World!" );
+        obj2.code();
+        System.out.println(obj1.age);
     }
 }
